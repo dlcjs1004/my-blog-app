0c53d2fed60500e4b8ec6b3964d279d152df88a2
dlcjs1004 <dlcjs1004@naver.com>
2024-12-28 13:51:29 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// 매번 렌더링 되기 전에 호출이 되는 파일
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react-scripts" />
+//typescript 타입 선언을 참조할 수 있도록 해주는 파일
