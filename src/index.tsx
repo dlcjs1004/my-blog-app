eb57bae0423cd0f45d2b1f3ad9a9d4e3fb336050
dlcjs1004 <dlcjs1004@naver.com>
2024-12-28 14:54:53 +0900

    react-router-dom 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
+// App.tsx와 public/index.html을 연결해주는 파일
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// App.tsx와 public/index.html을 연결해주는 파일
+import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
