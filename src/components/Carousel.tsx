4a4bf39842456dc481170c4b39da91af14bee023
dlcjs1004 <dlcjs1004@naver.com>
2024-12-28 21:03:54 +0900

    carousel 컴포넌트 작업

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+
+const IMAGE_1_URL = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const IMAGE_2_URL ="https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const IMAGE_3_URL ="https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=903&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+export default function Carousel() {
+  const [activeImage, setActiveImage] = useState(1);
+  // console.log(activeImage);
+
+  return (
+    <div>
+      <div className="carousel">
+        <ul className="carousel__slides">
+          {/* 현재 active한 image가 1일 경우 보여짐 */}
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-1"
+            checked={activeImage === 1}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img alt="scenery 1" src={IMAGE_1_URL} />
+            </div>
+            {/* 화살표 각 방향을 누를 경우_(왼쪽, 오른쪽) */}
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(3)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(2)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+
+          {/* 현재 active한 image가 2일 경우 보여짐 */}
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-2"
+            checked={activeImage === 2}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img alt="scenery 2" src={IMAGE_2_URL} />
+            </div>
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(1)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(3)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+
+          {/* 현재 active한 image가 3일 경우 보여짐 */}
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-3"
+            checked={activeImage === 3}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img alt="scenery 3" src={IMAGE_3_URL} />
+            </div>
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(2)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(1)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+
+          {/* 아래 점 3개를 눌렀을 각각의 경우 */}
+          <div className="carousel__dots">
+            <label
+              onClick={() => setActiveImage(1)}
+              className="carousel__dot"
+              id="img-dot-1"
+            ></label>
+            <label
+              onClick={() => setActiveImage(2)}
+              className="carousel__dot"
+              id="img-dot-2"
+            ></label>
+            <label
+              onClick={() => setActiveImage(3)}
+              className="carousel__dot"
+              id="img-dot-3"
+            ></label>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+}
