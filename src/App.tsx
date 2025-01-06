9e0d7fabf61e8f0436038f645db697594c89c13f
dlcjs1004 <dlcjs1004@naver.com>
2025-01-07 03:40:56 +0900

    다크모드 toggle css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // 매번 렌더링 되기 전에 호출이 되는 파일
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { app } from 'firebaseApp';
 import { getAuth, onAuthStateChanged } from 'firebase/auth'; //함수를 통해 사용자가 로그인이 되었는지의 유무를 알 수 있음
 import Router from './components/Router';
 import { ToastContainer, toast } from 'react-toastify';
 import Loader from 'components/Loader';
+import ThemeContext from 'context/ThemeContext';
 // import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
 
   const [init, setInit] = useState<boolean>(false);
@@ -30,12 +32,12 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
     <ToastContainer />
     {/* init이 된 경우에만 Home 페이지를 보여줄 수 있게 하고,
         아닌 경우에는 Loader 컴포넌트를 보여주도록 함 */}
     {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   ); //Router에 isAuthenticated의 props 전달
 }
 
